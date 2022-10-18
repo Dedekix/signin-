75b5008154fcadb7d9e8eea7ce97d55b2b797736
Delphine <iradukundadelphine8@gmail.com>
2022-10-18 22:06:59 +0200

    javascript

---
+++ ass2.js
@@ -0,0 +1,10 @@
+function Signin(){
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var o1=document.getElementById("o1");
+    var o2 = document.getElementById("o2");
+    o1.innerHTML="please enter your name";
+    o2.innerHTML="please enter your email";
+    document.getElementById("o1").style.color="red";
+    document.getElementById("o2").style.color="red";
+}
